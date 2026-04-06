16692820b0027603fbf7f87035a48b5a7486b751
Yash <yahoo.kr@gmail.com>
2026-04-06 18:06:34 -0400

    day 13

--- day13/day13_resnet_catdog.py
+++ day13/day13_resnet_catdog.py
@@ -176,3 +176,27 @@ if __name__ == '__main__':
     visualize_model(model_ft)
 
 
+    model_conv = torchvision.models.resnet18(weights='IMAGENET1K_V1')
+    for param in model_conv.parameters():
+        param.requires_grad = False
+
+    # Parameters of newly constructed modules have requires_grad=True by default
+    num_ftrs = model_conv.fc.in_features
+    model_conv.fc = nn.Linear(num_ftrs, 2)
+
+    model_conv = model_conv.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that only parameters of final layer are being optimized as
+    # opposed to before.
+    optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+    model_conv = train_model(model_conv, criterion, optimizer_conv,
+                         exp_lr_scheduler, num_epochs=25)
+    visualize_model(model_conv)
+
+    plt.ioff()
+    plt.show()
